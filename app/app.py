ee15aa87d211e8c1eff2767b368a92f556a6fb57
Lucas-Lee11 <lucasclee11@gmail.com>
2021-11-01 08:29:35 -0400

    app routing

--- app/app.py
+++ app/app.py
@@ -11,21 +11,53 @@ from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = 'forgotten charger'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def landing():
 
     # Check for session existance
-    if session.get('username') is not None:
-
+    if logged_in():
         return render_template('index.html')
     else:
-
         # If not logged in, show login page
         return render_template('intro.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    # Check for session existance
+    if logged_in():
+        return redirect(url_for('landing'))
+    else:
+        # If not logged in, show login page
+        return render_template('login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    # Check for session existance
+    if logged_in():
+        return redirect(url_for('landing'))
+    else:
+        # If not logged in, show login page
+        return render_template('register.html')
+
+@app.route('/<int:story_id>/edit')
+def edit_story(story_id):
     pass
+
+@app.route('/<int:story_id>')
+def view_story(story_id):
+    pass
+
+@app.route('/new')
+def new_story():
+    pass
+
+@app.errorhandler(404)
+def not_found():
+    pass
+
+def logged_in():
+    return session.get('username') is not None
+
 
 
 if __name__ == '__main__':
