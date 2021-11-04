4c710c265f0c17fd05c4c9d8cdcb2c4109e5e86e
Aryaman Goenka <arygoenka@gmail.com>
2021-11-03 20:32:57 -0400

    login and signup done

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 '''
-Forgotten Charger: Lewis Cass, Aryaman Goenka, Oscar Wang
+Forgotten Charger: Lewis Cass, Aryaman Goenka, Oscar Wang, Owen Yaggy
 Softdev
 P00: Cookie and Sessions Introduction
 2021-10-29
 time spent: 0.5
 '''
 import sqlite3
-
+import db_builder
 from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
@@ -22,7 +22,6 @@ def logged_in():
 
 @app.route('/', methods=['GET'])
 def landing():
-
     # Check for session existance
     if logged_in():
         return render_template('index.html')
@@ -32,34 +31,78 @@ def landing():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
     method = request.method
-
+    error = False
+    # Check for session existance
     if method == 'GET':
 
-        # Check for session existance
         if logged_in():
-            return redirect(url_for('landing'))
+            return render_template('index.html')
         else:
             # If not logged in, show login page
-            return render_template('login.html', error=False)
+            return render_template('login.html', error=error)
 
     if method == 'POST':
 
         # Get information from request.form since it is submitted via post
         username = request.form['username']
         password = request.form['password']
+        error = db_builder.login(username, password)
 
-        if username == TEMP_USER and password == TEMP_PASS:
+        if error:
+            # If incorrect, give feedback to the user
+            return render_template('login.html', error=error)
+        else:
             # Store user info into a cookie
             session['username'] = username
-            return redirect(url_for('landing'))
+            ##return redirect(url_for('landing'))
+            return render_template('index.html')
 
+@app.route('/register', methods=['GET','POST'])
+def register():
+    method = request.method
+    error = True
+    errormsg = ""
+    # Check for session existence
+    if method == "GET":
+        if logged_in():
+            return render_template('index.html')
+            ##return render_template('confirm_add.html')
         else:
+            # If not logged in, show login page
+            return render_template('register.html', error=error, errmsg=errormsg)
+            
+    if method == "POST":
+        new_username = request.form["new_username"]
+        new_password = request.form["new_password"]
+        confirm_password = request.form["confirm_password"]
 
-            # If incorrect, give feedback to the user
-            return render_template('login.html', error=True)
+        if confirm_password != new_password:
+            error = True
+            errormsg = "Error: Passwords do not match!"
+            return render_template("register.html", error=error, errmsg=errormsg)
+        if new_username == "":
+            error = True
+            errormsg = "Error: No username entered!"
+            return render_template("register.html", error=error, errmsg=errormsg)
+        elif new_password == "":
+            error = True
+            errormsg = "Error: No password entered!"
+            return render_template("register.html", error=error, errmsg=errormsg)
 
+        error, errormsg = db_builder.signup(new_username, new_password)
+
+        if error:
+            return render_template("register.html", error=error, errmsg=errormsg)
+        else: 
+            session['username'] = new_username
+            return render_template('index.html')
+
+        # sqlite stuff checking for username already exists
+        # sqlite stuff for submitting username and passowrd to the database
+
+    
+        
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
 
@@ -69,37 +112,6 @@ def logout():
 
     # After logout, return to login page
     return redirect(url_for('landing'))
-
-
-
-@app.route('/register', methods=['GET','POST'])
-def register():
-    method = request.method
-    # Check for session existence
-    if method == "GET":
-        if logged_in():
-            return redirect(url_for('landing'))
-        else:
-            # If not logged in, show login page
-            return render_template('register.html')
-
-    if method == "POST":
-        error = False
-        errormsg = ""
-        new_username = request.form["new_username"]
-        new_password = request.form["new_password"]
-        confirm_password = request.form["confirm_password"]
-
-        if confirm_password != new_password:
-            error = True
-            errormsg = "Error: Passwords do not match!"
-            return render_template("register.html", error=error, errmsg=errormsg)
-
-        # sqlite stuff checking for username already exists
-
-        # sqlite stuff for submitting username and passowrd to the database
-
-
 
 # For editing a particular story
 @app.route('/<int:story_id>/edit')
@@ -129,22 +141,13 @@ def add_story():
 
     return render_template(
         'confirm_add.html',
-
     )
-
 # Handles when a user visits a page without a route
 @app.errorhandler(404)
 def not_found(e):
     return render_template('404.html'), 404
 
-<<<<<<< HEAD
-=======
-# Utility function to check if there is a session
-def logged_in():
-    return session.get('username') is not None
-
-
->>>>>>> 966713f29632f52b12d27ca275d57d0292f4bd5b
 if __name__ == '__main__':
     app.debug = True
     app.run()
+    
--- app/db_builder.py
+++ app/db_builder.py
@@ -8,45 +8,82 @@ time spent: 0.5
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
-from flask import Flask, render_template, request, session, redirect, url_for
 
-DB_FILE="StoryCharger.db"
+def dbseteup():
+    DB_FILE="StoryCharger.db"
 
-db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
-c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+    c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
-#==========================================================
+    #==========================================================
 
-# < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
+    # < < < INSERT YOUR TEAM'S POPULATE-THE-DB CODE HERE > > >
 
-c.execute("Drop Table if exists Users")
-command = "Create Table Users (Username Text, Password Integer, UserID Primary Key)"    
-c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
-# run SQL statement
+    c.execute("Drop Table if exists Users")
+    command = "Create Table Users (Username Text, Password Integer)"    
+    c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
+    # run SQL statement
 
-c.execute("Drop Table if exists Stories")
-command = "Create Table Stories (Title Text, FullStory Text, Latest_Update Text, StoryID Primary Key)"    
-c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
-# run SQL statement
+    c.execute("Drop Table if exists Stories")
+    command = "Create Table Stories (Title Text, FullStory Text, Latest_Update Text, StoryID Primary Key)"    
+    c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
+    # run SQL statement
 
-c.execute("Drop Table if exists Contributions")
-command = "Create Table Contributions (UserID Integer, StoryID Integer, id Primary Key)"    
-c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
-# run SQL statement
+    c.execute("Drop Table if exists Contributions")
+    command = "Create Table Contributions (UserID Integer, StoryID Integer, id Primary Key)"    
+    c.execute(command)      # test SQL stmt in sqlite3 shell, save as string
+    # run SQL statement
 
-#==========================================================
+    #==========================================================
+    db.commit() #save changes
+    db.close()  #close database
 
-db.commit() #save changes
-db.close()  #close database
+def signup(username, password):
+    DB_FILE="StoryCharger.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 
-def signup():
     c = db.cursor()  
-    c.execute(("""SELECT username FROM Users WHERE username=?""",(username)))
+    ##dbseteup()
+
+    c.execute("""SELECT Username FROM Users WHERE Username=?""",[username])
     result = c.fetchone()
 
     if result:
-        prin()
+        return(True, "Username already exists")
     else: 
         c.execute("INSERT INTO Users VALUES (?, ?)", (username, password))
+        db.commit() 
+        db.close() 
+        return(False, "Welcome")
+
+
+def login(username, password):
+    DB_FILE="StoryCharger.db"
+    db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+
+    c = db.cursor()  
     
-    c.close()
+    c.execute("""SELECT Username FROM Users WHERE Username=? AND Password=?""",[username, password])
+    result = c.fetchone()
+
+    if result:
+        ##access this specifc user data
+        return(False)
+        
+    else: 
+        return(True)
+
+def new_story(title, story):
+    #c, db = setup()
+    DB_FILE = "StoryCharger.db"
+
+    db = sqlite3.connect(DB_FILE)  # open if file exists, otherwise create
+    c = db.cursor()
+    id = 0
+    for s in c.execute("SELECT StoryID FROM Stories"):
+        id += 1
+
+    c.execute("INSERT INTO Stories VALUES (?, ?, ?, ?)", (title, story, story, id + 1))
+    db.commit()
+    db.close()
+   
